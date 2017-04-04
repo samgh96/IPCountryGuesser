1b1d8b3436efc57c075849ec621dd382044edfe5
samgh96 <samgh96@gmail.com>
2017-04-04 17:17:26 +0200

    typos/elpy complains a lot

--- countryguesser.py
+++ countryguesser.py
@@ -1,6 +1,6 @@
-## La idea es coger una IP sacada de alguna parte, pasarla a decimal (de base 256)
-## y consultar en la base de datos el rango en el que está y así devolver el país para poder
-## hacer un block por country
+# La idea es coger una IP sacada de alguna parte, pasarla a decimal (de base 256)
+# y consultar en la base de datos el rango en el que está y así devolver el país para poder
+# hacer un block por country
 
 import csv
 import sys
@@ -8,37 +8,39 @@ import sys
 ipFile = sys.argv[1]
 ipDict = []
 
+
 def fileParser():
     with open(ipFile) as ipF:
         r = ipF.read()
         return r.splitlines()
 
-## OUT: Lista de diccionarios de entries ['ipStart', 'ipEnd', 'countryAcronym', 'countryName']
+
+# OUT: Lista de diccionarios de entries ['ipStart', 'ipEnd', 'countryAcronym', 'countryName']
 def csvToDict():
     with open('IP2LOCATION-LITE-DB1.CSV', 'r') as f:
         dReader = csv.DictReader(f)
         headers = dReader.fieldnames
         for line in dReader:
-            entry = {headers[0]: int(line[headers[0]]), # ipStart
-                    headers[1]: int(line[headers[1]]),  # ipEnd
-                    headers[2]: line[headers[2]],       # countryAcronym
-                    headers[3]: line[headers[3]]}       # countryName
-                
+            entry = {headers[0]: int(line[headers[0]]),  # ipStart
+                     headers[1]: int(line[headers[1]]),   # ipEnd
+                     headers[2]: line[headers[2]],        # countryAcronym
+                     headers[3]: line[headers[3]]}        # countryName
+
             ipDict.append(entry)
 
 
 def ipToDecimal(base256IP):
     splitIP = [int(x) for x in base256IP.split('.')]
-    decimalIP = (splitIP[0]*(256**3) +
-                    (splitIP[1]*(256**2)) +
-                    (splitIP[2]*(256**1)) +
-                    (splitIP[3]*(256**0)))
+    decimalIP = (splitIP[0] * (256**3) +
+                 (splitIP[1] * (256 ** 2)) +
+                 (splitIP[2] * (256 ** 1)) +
+                 (splitIP[3] * (256 ** 0)))
     return decimalIP
-    
+
 
 def isIPinRange(decimalIP, dictEntry):
-    return (decimalIP >= dictEntry['ipStart']) and (decimalIP <= dictEntry['ipEnd']) 
-    
+    return (decimalIP >= dictEntry['ipStart']) and (decimalIP <= dictEntry['ipEnd'])
+
 
 csvToDict()
 
